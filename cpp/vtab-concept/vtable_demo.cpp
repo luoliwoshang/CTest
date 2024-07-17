25db6f44b723f2ea54bc5ad2d14b5cfc8d28b0b0
luoliwoshang <2643523683@qq.com>
2024-07-17 11:38:17 +0800

    cpp:vtable

--- cpp/vtab-concept/vtable_demo.cpp
+++ cpp/vtab-concept/vtable_demo.cpp
@@ -6,7 +6,7 @@ class Base
 public:
     virtual void func()
     {
-        std::cout << "[Base] Function called" << std::endl;
+        std::cout << "Base function" << std::endl;
     }
 };
 
@@ -16,16 +16,28 @@ class Derived : public Base
 public:
     void func() override
     {
-        std::cout << "[Derived] Function called" << std::endl;
+        std::cout << "Derived function" << std::endl;
     }
 };
 
+// 打印vtable和调用第一个虚函数
 void printVtable(void *obj)
 {
-    // 假设vptr在对象的起始位置
+    // 将 obj 解释为双重指针，并解引用获取 vptr
     void **vptr = *(void ***)obj;
-    std::cout << "Vtable address: " << vptr << std::endl;
-    std::cout << "First function address: " << vptr[0] << std::endl;
+    // vptr 是该对象的第一个成员，其地址为obj的地址，其值为vtable的地址
+
+    std::cout << "Object address: " << obj << std::endl;  // Base:0x16b0aaca0 Derived:0x16b0aac98
+    std::cout << "Vptr address: " << &vptr << std::endl;  // 均为同一个 Vptr address: 0x16b0aac50 ，为什么这里的 Vptr 的地址比 Object 地址小 80 字节
+    std::cout << "Vtable address: " << vptr << std::endl; // Base: 0x1005cc0b0 Derived: 0x1005cc0d8
+
+    // 将vptr转换为具体的函数指针类型指针
+    using FuncPointer = void (*)(void);
+    FuncPointer *funcPtr = (FuncPointer *)vptr;
+    std::cout << "First function address in vtable: " << (void *)funcPtr[0] << std::endl;
+
+    // 调用第一个虚函数
+    funcPtr[0]();
 }
 
 int main()
