0a3875f45daf4051367b771f4e89b92b90a1ac36
luoliwoshang <2643523683@qq.com>
2024-07-28 09:47:19 +0800

    c/lua:metatable

--- c/lua/lua-c/6-metatable/demo.c
+++ c/lua/lua-c/6-metatable/demo.c
@@ -1,34 +1,45 @@
+#include <lauxlib.h>
 #include <lua.h>
 #include <lualib.h>
-#include <lauxlib.h>
+#include <stdio.h>
 
-/* `__index` 元方法的实现 */
-int index_meta(lua_State *L) {
-    lua_pushstring(L, "This is a default value");
-    return 1;  
+// 简单的元方法：打印消息
+static int l_tostring(lua_State *L) {
+    lua_pushstring(L, "Hello from metatable!");
+    return 1;
 }
 
-int main(void) {
+int main() {
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
 
-    // 创建一个新表
+    // 创建主表
     lua_newtable(L);
 
-    // 为表创建一个元表
+    // 创建元表
     lua_newtable(L);
 
-    lua_pushstring(L, "__index");
-    lua_pushcfunction(L, index_meta);  
-    lua_settable(L, -3); 
+    // 设置 __tostring 元方法
+    lua_pushcfunction(L, l_tostring);
+    lua_setfield(L, -2, "__tostring");
 
-    // 将元表设置到之前创建的表上
+    // 将元表设置为主表的元表
     lua_setmetatable(L, -2);
 
-    // 模拟访问不存在的键
-    lua_pushstring(L, "testKey");
-    lua_gettable(L, -2);  // 触发 __index 元方法
-    printf("Result of access: %s\n", lua_tostring(L, -1));  // 输出返回的默认值
+    // 将主表设置为全局变量 "obj"
+    lua_setglobal(L, "obj");
+
+    // 执行Lua代码来测试
+    const char *testcode = "local mt = getmetatable(obj)\n"
+                           "if mt and mt.__tostring then\n"
+                           "    print(mt.__tostring(obj))\n"
+                           "else\n"
+                           "    print('Metatable or __tostring not set correctly')\n"
+                           "end\n";
+
+    if (luaL_dostring(L, testcode) != LUA_OK) {
+        fprintf(stderr, "Error: %s\n", lua_tostring(L, -1));
+    }
 
     lua_close(L);
     return 0;
