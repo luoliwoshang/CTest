ab19629e8b48b92a47c5353f2a0439b99d98b325
luoliwoshang <2643523683@qq.com>
2024-11-11 19:48:09 +0800

    clang:macro preprocess skip:

--- c/clang/preprocessor/definepreprocess/main.c
+++ c/clang/preprocessor/definepreprocess/main.c
@@ -14,13 +14,23 @@
 #define B 3
 #endif
 
+#ifdef __BLOCKS__
+void debug_func(void);
+#endif
+
+#ifdef not_defined
+void not_defined_func(void);
+#endif
+
 int main() { return 0; }
-// clang -E -dM main.c
+// clang -E main.c
+// llcppsigfetch --extract "./main.c" -temp=false -cpp=false -out=true
 // #define A 1
 // #define OS "unix" (因为 WINDOWS 未定义)
 // #define B 2 (因为 A 等于 1)
 // 不会看到 #define OS "windows" 或 #define B 3
 
-// 首先解析所有条件指令
-// 根据条件判断保留或移除相关代码块
-// 只有实际生效的宏定义才会被显示
+// 并且对于 #ifdef __BLOCKS__ 的宏定义，对应的
+// debug_func 由于默认__BLOCKS__是开启状态，所以也会输出这个定义，对于clang -E 的输出以及 llcppsigfetch
+// 的输出，都会输出这个定义 并对于 not_defined 对应的代码块，由于并没有声明该宏定义，所以不会输出 not_defined_func
+// 的定义
